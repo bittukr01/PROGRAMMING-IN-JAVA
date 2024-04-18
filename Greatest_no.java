5e493aa5513cc9262683eb7432846287f500638b
Bittukumar0610 <bittu.c.2424@iibm.in>
2024-04-18 09:09:36 +0530

    some modified

--- Greatest_no.java
+++ Greatest_no.java
@@ -1,29 +1,26 @@
+
+//find the greatest_no
 import java.util.*;
+
 public class Greatest_no {
     public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         System.out.println("Enter first Number : ");
-        int num1=s.nextInt();
+        int num1 = s.nextInt();
         System.out.println("Enter second Number : ");
-        int num2=s.nextInt();
+        int num2 = s.nextInt();
         System.out.println("Enter third Number : ");
-        int num3=s.nextInt();
+        int num3 = s.nextInt();
 
-        if(num1>num2 && num1>num3){
+        if (num1 > num2 && num1 > num3) {
             System.out.println("Greatest number is: " + num1);
+        } else if (num2 > num1 && num2 > num3) {
+            System.out.println("Greatest number is: " + num2);
+        } else if (num3 > num1 && num3 > num2) {
+            System.out.println("Greatest number is: " + num3);
+        } else {
+            System.out.println(" All Equal");
         }
-            else if(num2>num1 && num2>num3){
-                System.out.println("Greatest number is: " + num2);
-            }
-            else if(num3>num1 && num3>num2){
-                System.out.println("Greatest number is: " + num3);
-            }
-        else{
-                System.out.println(" All Equal" );
-            }
-        }
-
-        
     }
-    
 
+}
